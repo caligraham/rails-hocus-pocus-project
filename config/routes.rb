6dbfd2433170319bac63000b10bac8c683f55dd9
Rebecca <graham.cali@gmail.com>
2021-01-23 21:40:33 -0600

    updating routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,11 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-  
+  resources :users
   
   resources :crystal_balls
   resources :spells
   resources :spellbooks
-  resources :users
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
